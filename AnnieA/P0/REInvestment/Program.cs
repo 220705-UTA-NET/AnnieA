54bf2afd0d4a3035231e3c78bbb27805311d993a
annie872@revature.net <annie872@revature.net>
2022-07-25 06:15:46 -0700

    Updates on Project zero 7/25/22

---
+++ AnnieA/P0/REInvestment/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace REInvestment
+{
+    class Program
+    {
+        // method
+
+        static void Main()
+        {
+            
+            Analyzer oAnalyzer = new Analyzer();
+            oAnalyzer.StartNewDealAnalysis();
+        }
+    }
+}
